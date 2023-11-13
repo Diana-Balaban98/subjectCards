3b7ecf727dff0cf1e019556b640912b9f8f39537
Diana-Balaban98 <diana.rud.2012@mail.ru>
2023-11-13 22:11:16 +0300

    add hooks

---
+++ src/hooks/useAppSelector.ts
@@ -0,0 +1,5 @@
+import { TypedUseSelectorHook, useSelector } from 'react-redux'
+
+import { AppRootState } from '../state/store'
+
+export const useAppSelector: TypedUseSelectorHook<AppRootState> = useSelector
